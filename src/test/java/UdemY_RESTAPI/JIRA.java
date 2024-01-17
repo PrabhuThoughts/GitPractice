b053d35af9c6c9b89bd9bf8021515074aae96092
Shanthana prabhu <shanthanaprabhubalamurugan@gmail.com>
2024-01-17 20:16:36 +0530

    add extra sys out

--- src/test/java/UdemY_RESTAPI/JIRA.java
+++ src/test/java/UdemY_RESTAPI/JIRA.java
@@ -3,6 +3,8 @@ package UdemY_RESTAPI;
 public class JIRA {
     public static void main(String[] args) {
         System.out.println("hi from JIRA java class");
+        System.out.println("hi from JIRA java class");
+        System.out.println("hi from JIRA java class");
     }
 
 }
